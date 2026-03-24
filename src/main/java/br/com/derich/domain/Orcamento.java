8d2057ab77fa70a8648434a41bfe691b99942382
Derich Rosario <derich.rosario22@gmail.com>
2026-03-23 21:38:53 -0300

    feat(Orcamento) adicionar métodos de calcular dinamicamente campos  com orientação a dominio

--- src/main/java/br/com/derich/domain/Orcamento.java
+++ src/main/java/br/com/derich/domain/Orcamento.java
@@ -7,6 +7,10 @@ import lombok.Setter;
 import java.math.BigDecimal;
 import java.util.List;
 
+/*
+O lombok só cria automaticamente os getters e setters caso eles não existam no código
+por isso o getSaldoDisponivel pode ser declarado sem gerar conflitos com a anotação
+ */
 @Getter
 @Setter
 @Entity
@@ -25,9 +29,46 @@ public class Orcamento {
     @Column(nullable = false)
     private BigDecimal salario;
 
-    @Column(nullable = false)
     private BigDecimal totalGasto;
 
-    @Column(nullable = false)
     private BigDecimal saldoDisponivel;
+
+    public BigDecimal getTotalGasto(){
+        return this.getCompras().stream()
+                .map(Compra::getValor)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    /**
+     * O saldo disponível deve ser calculado a partir dos campos salário e totalGasto
+     * por conta disso é melhor que ele seja calculado no momento que é chamado
+     * ao invés de ficar salvando no banco de dados sempre que algum desses outros campos for alterado
+     */
+    public BigDecimal getSaldoDisponivel() {
+        return salario.subtract(totalGasto);
+    }
+
+    /*
+        Orientado a domínio = objeto controla o seu próprio comportamento
+
+        Não é possível quebrar as regras para adicionar uma compra
+        Lógica centralizada
+
+        | Tipo de lógica          | Vai onde? | Exemplo          |
+        | ----------------------- | --------- | ---------------- |
+        | Validação de compra     | Entidade  | valor > 0        |
+        | Cálculo de saldo        | Entidade  | salario - gastos |
+        | Adicionar item na lista | Entidade  | compras.add      |
+        | Buscar no banco         | Service   | findById         |
+        | Salvar dados            | Service   | save             |
+        | Fluxo entre objetos     | Service   | transferir       |
+        | Integração externa      | Service   | API, fila, etc   |
+
+        Essa regra precisa de algo fora do objeto?
+        Não → Entidade
+        Sim → Service
+     */
+    public void adicionarCompra(Compra compra){
+        this.getCompras().add(compra);
+    }
 }
