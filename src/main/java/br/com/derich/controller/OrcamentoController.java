7e67dfe088ae0a4662e3914d224a6b2ccb53dc14
Derich Rosario <derich.rosario22@gmail.com>
2026-04-19 01:55:15 -0300

    feat(orcamentoController) adicionar anotação Valid no método salvarOrcamento

--- src/main/java/br/com/derich/controller/OrcamentoController.java
+++ src/main/java/br/com/derich/controller/OrcamentoController.java
@@ -2,6 +2,7 @@ package br.com.derich.controller;
 
 import br.com.derich.domain.Orcamento;
 import br.com.derich.service.OrcamentoService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class OrcamentoController {
     private OrcamentoService orcamentoService;
 
     @PostMapping
-    public ResponseEntity<Orcamento> salvarOrcamento(@RequestBody Orcamento orcamento){
+    public ResponseEntity<Orcamento> salvarOrcamento(@RequestBody @Valid Orcamento orcamento){
         Orcamento orcamentoSalvo = orcamentoService.salvarOrcamento(orcamento);
         return ResponseEntity.status(HttpStatus.CREATED).body(orcamentoSalvo);
     }
