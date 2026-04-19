731d8c7b0536dc47c94d1a75e8e39bf3d1165590
Derich Rosario <derich.rosario22@gmail.com>
2026-04-19 01:54:36 -0300

    feat(orcamentoController) adicionar anotação RequestBody no parâmetro do método salvarOrcamento

--- src/main/java/br/com/derich/controller/OrcamentoController.java
+++ src/main/java/br/com/derich/controller/OrcamentoController.java
@@ -5,6 +5,7 @@ import br.com.derich.service.OrcamentoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class OrcamentoController {
     private OrcamentoService orcamentoService;
 
     @PostMapping
-    public ResponseEntity<Orcamento> salvarOrcamento(Orcamento orcamento){
+    public ResponseEntity<Orcamento> salvarOrcamento(@RequestBody Orcamento orcamento){
         Orcamento orcamentoSalvo = orcamentoService.salvarOrcamento(orcamento);
         return ResponseEntity.status(HttpStatus.CREATED).body(orcamentoSalvo);
     }
