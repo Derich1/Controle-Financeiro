cd0e35977dc8eed1c39069b821ff9c5672e58ca8
Derich Rosario <derich.rosario22@gmail.com>
2026-04-19 01:37:03 -0300

    feat(orcamentoController) adicionar método de salvar orçamento

--- src/main/java/br/com/derich/controller/OrcamentoController.java
+++ src/main/java/br/com/derich/controller/OrcamentoController.java
@@ -1,5 +1,9 @@
 package br.com.derich.controller;
 
+import br.com.derich.domain.Orcamento;
+import br.com.derich.service.OrcamentoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/orcamento")
 public class OrcamentoController {
 
+    private OrcamentoService orcamentoService;
 
+    public ResponseEntity<Orcamento> salvarOrcamento(Orcamento orcamento){
+        Orcamento orcamentoSalvo = orcamentoService.salvarOrcamento(orcamento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orcamentoSalvo);
+    }
 }
