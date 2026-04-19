c29646b2eb638d0a671f064fe3a85c90b7ce6792
Derich Rosario <derich.rosario22@gmail.com>
2026-04-19 01:26:27 -0300

    feat(orcamentoRepository) adicionar método de buscar orçamento pelo mês do ano

--- src/main/java/br/com/derich/repository/IOrcamentoRepository.java
+++ src/main/java/br/com/derich/repository/IOrcamentoRepository.java
@@ -3,7 +3,10 @@ package br.com.derich.repository;
 import br.com.derich.domain.Orcamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Month;
 import java.util.Optional;
 
 public interface IOrcamentoRepository extends JpaRepository<Orcamento, String> {
+
+    Optional<Orcamento> findByMonth(Month mes);
 }
