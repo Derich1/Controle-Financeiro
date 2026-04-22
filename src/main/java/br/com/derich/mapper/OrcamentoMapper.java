09d3d7f20654507f0a0db3847203768ec7aae92b
Derich Rosario <derich.rosario22@gmail.com>
2026-04-21 21:47:53 -0300

    feat(OrcamentoMapper) adicionar método de mapear entidade para DTO de resposta

--- src/main/java/br/com/derich/mapper/OrcamentoMapper.java
+++ src/main/java/br/com/derich/mapper/OrcamentoMapper.java
@@ -2,10 +2,12 @@ package br.com.derich.mapper;
 
 import br.com.derich.domain.Orcamento;
 import br.com.derich.dto.OrcamentoRequestDTO;
+import br.com.derich.dto.OrcamentoResponseDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface OrcamentoMapper {
 
     Orcamento toEntity(OrcamentoRequestDTO dto);
+    OrcamentoResponseDTO toResponse(Orcamento dto);
 }
