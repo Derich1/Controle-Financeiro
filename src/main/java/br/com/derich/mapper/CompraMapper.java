4a4c9fc4745e9b55e81c7efd7480ebcafe983995
Derich Rosario <derich.rosario22@gmail.com>
2026-05-01 19:10:24 -0300

    feat(CompraMapper) adicionar método para transformar lista de entidade em lista de DTO

--- src/main/java/br/com/derich/mapper/CompraMapper.java
+++ src/main/java/br/com/derich/mapper/CompraMapper.java
@@ -1,11 +1,15 @@
 package br.com.derich.mapper;
 
 import br.com.derich.domain.Compra;
-import br.com.derich.dto.CompraRequestDTO;
+import br.com.derich.dto.CompraDTO;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CompraMapper {
 
-    Compra toEntity(CompraRequestDTO dto);
+    Compra toEntity(CompraDTO dto);
+    CompraDTO toDTO(Compra compra);
+    List<CompraDTO> toDTOList(List<Compra> compras);
 }
