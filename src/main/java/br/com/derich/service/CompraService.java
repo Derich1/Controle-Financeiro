a9b290f041c039428f63a1d212565d3f054cf49f
Derich Rosario <derich.rosario22@gmail.com>
2026-05-01 19:11:18 -0300

    refactor(CompraService) utilizar dto ao invés da entidade no método salvar compra

--- src/main/java/br/com/derich/service/CompraService.java
+++ src/main/java/br/com/derich/service/CompraService.java
@@ -1,6 +1,8 @@
 package br.com.derich.service;
 
 import br.com.derich.domain.Compra;
+import br.com.derich.dto.CompraDTO;
+import br.com.derich.mapper.CompraMapper;
 import br.com.derich.repository.ICompraRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,13 +16,17 @@ import static br.com.derich.util.MergeUtils.mergeIfDifferent;
 public class CompraService {
 
     public ICompraRepository compraRepository;
+    public CompraMapper compraMapper;
 
-    public CompraService(ICompraRepository compraRepository){
+    public CompraService(ICompraRepository compraRepository, CompraMapper compraMapper){
         this.compraRepository = compraRepository;
+        this.compraMapper = compraMapper;
     }
 
-    public Compra salvarCompra(Compra compra) {
-        return compraRepository.save(compra);
+    public CompraDTO salvarCompra(CompraDTO dto) {
+        Compra compra = compraMapper.toEntity(dto);
+        Compra compraSalva = compraRepository.save(compra);
+        return compraMapper.toDTO(compraSalva);
     }
 
     public List<Compra> listarCompras(){
