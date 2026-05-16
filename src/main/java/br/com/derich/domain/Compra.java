451fcd0666d9566cd7a3ea1a88f5845bab6b4202
Derich Rosario <derich.rosario22@gmail.com>
2026-05-16 19:14:59 -0300

    chore(Compra) remover imports não utilizados

--- src/main/java/br/com/derich/domain/Compra.java
+++ src/main/java/br/com/derich/domain/Compra.java
@@ -3,7 +3,6 @@ package br.com.derich.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
