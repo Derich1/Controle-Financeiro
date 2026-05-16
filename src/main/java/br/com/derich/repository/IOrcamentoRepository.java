76e0abd802dabd0301e2d4d64e50f4e41c6afea0
Derich Rosario <derich.rosario22@gmail.com>
2026-05-16 19:12:15 -0300

    fix(IOrcamentoRepository) chamar método corretamente, alterado de findByMonth para findByMes

--- src/main/java/br/com/derich/repository/IOrcamentoRepository.java
+++ src/main/java/br/com/derich/repository/IOrcamentoRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface IOrcamentoRepository extends JpaRepository<Orcamento, String> {
 
-    Optional<Orcamento> findByMonth(Month mes);
+    Optional<Orcamento> findByMes(Month mes);
 }
