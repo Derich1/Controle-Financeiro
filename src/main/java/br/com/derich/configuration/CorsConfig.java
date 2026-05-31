bd88be55c76f24030ebac9c312feb26b84cb7aa0
Derich Rosario <derich.rosario22@gmail.com>
2026-05-31 11:00:50 -0300

    feat(CorsConfig) adicionar CORS para frontend fazer chamadas

---
+++ src/main/java/br/com/derich/configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package br.com.derich.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://controle-financeiro-frontend-202m.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
